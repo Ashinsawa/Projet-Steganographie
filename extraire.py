304d64526a27f49d7304a99d02a70a7d7a2230a0
Ashinsawa <tom.philippe2007@gmail.com>
2026-02-19 16:40:07 +0100

    pareil et faute coriger

--- extraire.py
+++ extraire.py
@@ -1,7 +1,8 @@
 from PIL import Image
 
 def extraire_message(image_path):
-    img = Image.open(image_path)
+    # On force en RGBA pour être sûr d'avoir 4 valeurs
+    img = Image.open(image_path).convert("RGBA")
     pixels = img.load()
     width, height = img.size
     
@@ -11,17 +12,15 @@ def extraire_message(image_path):
     
     for y in range(height):
         for x in range(width):
-            r, g, b = pixels[x, y]
+            r, g, b, a = pixels[x, y]
             
-            # On récupère le bit de poids faible (LSB) avec l'opérateur ET
+            # On récupère le bit de poids faible (LSB) du rouge
             bits_extraits += str(r & 1)
             
-            # On vérifie si on a trouvé le marqueur de fin
+            # Si on a trouvé le marqueur de fin, on reconstruit le message
             if bits_extraits.endswith(marqueur_fin):
-                # On retire le marqueur pour ne garder que les données
                 bits_utiles = bits_extraits[:-len(marqueur_fin)]
                 
-                # On regroupe par 8 bits pour reformer les caractères
                 for i in range(0, len(bits_utiles), 8):
                     octet = bits_utiles[i:i+8]
                     message_final += chr(int(octet, 2))
@@ -31,4 +30,4 @@ def extraire_message(image_path):
     return "Aucun marqueur de fin trouvé."
 
 # Exemple d'utilisation :
-print(extraire_message("images/image1_codee_codee.png"))
+print(extraire_message("images/image1_codee.png"))
